3fbdf2a4dfbfd1eed44d8a3d3bec25b4d45cb4ed
Hanine-Benfarhat <hanine.benfarhat@outlook.com>
2025-08-14 14:50:17 +0100

    EF Core integration
    - Updated `AuthApp.API.csproj` to include `UserSecretsId` and new EF Core packages.
    - Modified `Program.cs` to add `AppDbContext` and removed the weather forecast endpoint.
    - Adjusted `appsettings.json` formatting by removing "AllowedHosts".
    - Updated `AuthApp.Domain.csproj` with EF Core references and added a data folder.
    - Removed unnecessary `Class1.cs` file.
    - Created `RefreshToken` and `User` classes for user management.
    - Defined `AppDbContext` for database interactions.
    - Added migration files to establish initial database schema for `Users` and `RefreshTokens`.

---
+++ AuthApp.Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthApp.Domain.Entities;
+
+public  class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string UserName { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "User";
+    public string Email { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? LastLoginAt { get; set; }
+    public List<RefreshToken> RefreshTokens { get; set; } = new();
+}
---
+++ AuthApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace AuthApp.Infrastructure.Data;
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.RefreshTokens)
+            .WithOne(rt => rt.User)
+            .HasForeignKey(rt => rt.UserId);
+        base.OnModelCreating(modelBuilder);
+    }
+}
